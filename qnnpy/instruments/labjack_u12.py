3c22cd75d670ca2789715885e8c4c840879accdd
Emma Batson <emmakbat@gmail.com>
2021-11-15 10:15:35 -0500

    non-debugged muxed temperature sweep

--- qnnpy/instruments/labjack_u12.py
+++ qnnpy/instruments/labjack_u12.py
@@ -8,6 +8,8 @@ class LabJackU12(object):
     """
     def __init__(self, debug=False):
         self.device = u12.U12(debug)
+        self.read_digital(0)
+        # note: may need to collect calibration data?
 
     def write_digital(self, channel, state):
         self.device.eDigitalOut(channel, state)
@@ -19,3 +21,17 @@ class LabJackU12(object):
     def read_analog(self, channel):
         read_info = self.device.eAnalogIn(channel)
         return read_info['voltage']
+
+    def set_mux_channel(self, mux_channel):
+        '''
+        mux_channel: channel to switch mux to (base ten)
+
+        writes the number corresponding to the mux channel
+        to LabJack digital outputs in binary, starting from 
+        IO0
+        '''
+        mux_bin = bin(mux_channel)[2:]
+        # reverse endianness (direction)
+        mux_bin_little = mux_bin[::-1]
+        for i in range(len(mux_bin_little)):
+            self.write_digital(i, mux_bin_little[i])
