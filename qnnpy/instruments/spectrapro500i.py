d57238cc76b12b6ebf877562d221e86c43ac0713
Maurice Krielaart <esakrielaart@hotmail.com>
2022-02-10 14:25:53 -0500

    Setting up environment for spectrometer methods, based on Instrument class.

---
+++ qnnpy/instruments/spectrapro500i.py
@@ -0,0 +1,25 @@
+# Add this directory to the environment for debugging.
+if __name__ == '__main__':
+    import sys
+    sys.path.append('.')
+
+# Add all run-time requirements.
+from numpy import void
+from qnnpy.instruments.instrument import Instrument
+
+# Support for bytes
+from numpy import byte
+
+# Class SpectraPro500i
+# TODO documentation.
+class SpectraPro500i(Instrument):
+    def __init__(self, termination_character: byte = b'\x0a', port: str = ''):
+        super().__init__(termination_character=termination_character, port=port)
+        
+
+# Local debugging scripts, will only run when directly running this script.
+if __name__ == '__main__':
+    sp = SpectraPro500i()
+    sp.connect('COM1')
+    print(sp.status)
+    print(sp.link)
