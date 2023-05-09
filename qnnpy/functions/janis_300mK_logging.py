8bbe4031d5940c9a8f25bfb9ac354b4eb524fea3
omedeiro <omedeiro@mit.edu>
2023-05-09 12:23:27 -0400

    Update janis_300mK_logging.py

--- qnnpy/functions/janis_300mK_logging.py
+++ qnnpy/functions/janis_300mK_logging.py
@@ -18,18 +18,30 @@ from qnnpy.instruments.lakeshore336 import Lakeshore336
 
 
 while True:        
-
-    ls1 = Lakeshore336('GPIB0::13::INSTR')
-    
-    stage_3K = ls1.read_temp('D4')
-    stage_40K = ls1.read_temp('D2')
-    stage_SORB = ls1.read_temp('D3')
-    stage_He3Pot = ls1.read_temp('D1')
+    try:    
+        ls1 = Lakeshore336('GPIB0::13::INSTR')
+    except:
+        print('failed connection to Lakeshore336')
+        
+    try:
+        stage_3K = ls1.read_temp('D4')
+        sleep(2)
+        stage_40K = ls1.read_temp('D2')
+        sleep(2)
+        stage_SORB = ls1.read_temp('D3')
+        sleep(2)
+        stage_He3Pot = ls1.read_temp('D1')
+        sleep(1)
+        # print([stage_3K, stage_40K, stage_SORB, stage_He3Pot])
+    except:
+        print('failed to read temp')
     # ls1.read_temp('D')
     
-    
-    qf.log_data_to_database(table_name='janis_300mK_logging', stage_3K=stage_3K, stage_40K=stage_40K, stage_SORB=stage_SORB, stage_He3Pot=stage_He3Pot)
-    
+    try:
+        qf.log_data_to_database(table_name='janis_300mK_logging', stage_3K=stage_3K, stage_40K=stage_40K, stage_SORB=stage_SORB, stage_He3Pot=stage_He3Pot)
+    except:
+        print('failed to write to database')
+        
     sleep(1)
     
     
